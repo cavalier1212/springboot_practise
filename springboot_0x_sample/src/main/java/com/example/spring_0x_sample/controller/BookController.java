be0049287c43517870d29c7941dbaa0606282b0c
cavalier1212 <cavalier121@gmail.com>
2023-05-01 20:36:12 +0800

    feat: add simple log

--- springboot_0x_sample/src/main/java/com/example/spring_0x_sample/controller/BookController.java
+++ springboot_0x_sample/src/main/java/com/example/spring_0x_sample/controller/BookController.java
@@ -1,5 +1,8 @@
 package com.example.spring_0x_sample.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/books")
 public class BookController {
+
+    private static final Logger log = LoggerFactory.getLogger(BookController.class);
     
     @GetMapping
     public String getById() {
+        log.info("info..");
+        log.debug("debug..");
+        log.warn("warn...");
+        log.error("error...");
         return "SpringBoot Running...";
     }
 }
